e27fe1192d2d7ec77860aad9162b03d71b729490
liyanjun <liyanjun@do-global.com>
2021-08-10 19:06:14 +0800

    test(Sort): add merge & shell sort test

--- src/algorithms/sort/__test__/Sort.test.js
+++ src/algorithms/sort/__test__/Sort.test.js
@@ -1,17 +1,32 @@
 import quickSort from '../quickSort'
 import insertSort from '../insertSort'
+import shellSort from '../shellSort'
+import mergeSort from '../mergeSort'
 
-let array = [6, 8, 4, 7, 9, 3, 5, 1]
+let array = [6, 8, 4, 7, 3, 5, 1]
 describe('quick sort', () => {
     it('should', () => {
         let sortedArr = quickSort(array)
-        expect(sortedArr).toEqual([1, 3, 4, 5, 6, 7, 8, 9])
+        expect(sortedArr).toEqual([1, 3, 4, 5, 6, 7, 8])
     })
 })
 
 describe('insert sort', () => {
     it('should', () => {
         let sortedArr = insertSort(array)
-        expect(sortedArr).toEqual([1, 3, 4, 5, 6, 7, 8, 9])
+        expect(sortedArr).toEqual([1, 3, 4, 5, 6, 7, 8])
+    })
+})
+
+describe('shell sort', () => {
+    it('should', () => {
+        let sortedArr = shellSort(array)
+        expect(sortedArr).toEqual([1, 3, 4, 5, 6, 7, 8])
+    })
+})
+describe('merge sort', () => {
+    it('should', () => {
+        let sortedArr = mergeSort(array)
+        expect(sortedArr).toEqual([1, 3, 4, 5, 6, 7, 8])
     })
 })
